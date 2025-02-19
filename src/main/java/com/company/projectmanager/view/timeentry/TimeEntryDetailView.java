ede1351da83f79ce33b3baaae683bbce9970dd76
andrew-win <andreynizovkin@inbox.ru>
2025-02-19 16:53:22 +0300

    Work: download and view files

--- src/main/java/com/company/projectmanager/view/timeentry/TimeEntryDetailView.java
+++ src/main/java/com/company/projectmanager/view/timeentry/TimeEntryDetailView.java
@@ -1,9 +1,12 @@
 package com.company.projectmanager.view.timeentry;
 
 import com.company.projectmanager.entity.TimeEntry;
+import com.company.projectmanager.entity.User;
 import com.company.projectmanager.view.main.MainView;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.flowui.view.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 
@@ -13,8 +16,14 @@ import java.time.LocalDateTime;
 @EditedEntityContainer("timeEntryDc")
 @DialogMode(width = "AUTO", height = "AUTO")
 public class TimeEntryDetailView extends StandardDetailView<TimeEntry> {
+
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
     @Subscribe
     public void onInitEntity(final InitEntityEvent<TimeEntry> event) {
         event.getEntity().setEntryDate(LocalDateTime.now());
+        final User user = (User) currentAuthentication.getUser();
+        event.getEntity().setUser(user);
     }
 }
