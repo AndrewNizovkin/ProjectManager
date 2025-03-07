2c95ed93cfa6ae06bf0de33bf8bb2a8031db35f4
andrew-desktop <andreynizovkin@inbox.ru>
2025-03-07 11:27:47 +0300

    Fix: changes db paramerers

--- run-postgres.sh
+++ run-postgres.sh
@@ -1,1 +1,1 @@
-docker run -d --name postgres-container -e POSTGRES_DB=projectdb -e POSTGRES_USER=andrew -e POSTGRES_PASSWORD=1234 -p 5432:5432 -v /home/andrew/projectdb:/var/lib/postgresql/data postgres:alpine3.19
+docker run -d --name postgres-container -e POSTGRES_DB=projectdb -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -p 5432:5432 -v /home/andrew/projectdb:/var/lib/postgresql/data postgres:alpine3.19
